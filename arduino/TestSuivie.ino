11cc6438cf1e242a72258d91f544e6f263becfb9
HugoLeBoennec <33154707+HugoLeBoennec@users.noreply.github.com>
2018-06-19 16:29:54 +0200

    Update TestSuivie.ino

--- arduino/TestSuivie.ino
+++ arduino/TestSuivie.ino
@@ -11,6 +11,7 @@ int AG = 0;
 int AD = 0;
 int Dir = 0;
 int Solution = 0;
+int reflex = 0;
 #define I2C_ADDRESS 0x0f
 void setup() {
   // put your setup code here, to run once:
@@ -23,12 +24,25 @@ void setup() {
 
 void loop() {
 
-
-    
-  if(HIGH == digitalRead(signalPinAA)){
+ /* if(HIGH == digitalRead(signalPinAA)){ 
   Dir = 3;
   }
 
+  if(HIGH == digitalRead(signalPinAD)){
+  Dir = 2;
+  }
+  
+  if(HIGH == digitalRead(signalPinAG)){
+  Dir = 1;
+  }*/
+
+  
+ if((HIGH == digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAG) && HIGH != digitalRead(signalPinAD)) || (HIGH != digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH != digitalRead(signalPinAD)) || (HIGH != digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD))){
+  
+  Serial.println("Virage");
+  if(HIGH == digitalRead(signalPinAA)){ 
+  Dir = 3;
+  }
 
   if(HIGH == digitalRead(signalPinAD)){
   Dir = 2;
@@ -37,36 +51,95 @@ void loop() {
   if(HIGH == digitalRead(signalPinAG)){
   Dir = 1;
   }
+ }
 
-  if(HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAD)){
+ 
+ if((HIGH == digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH != digitalRead(signalPinAD)) || (HIGH != digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD)) || (HIGH == digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD))){
+  
+  Serial.println("Croisement en T");
+  if(HIGH == digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH != digitalRead(signalPinAD)){
+  Serial.println("-|");
+  Solution = random(1,2);
+  if(Solution == 2){ 
+  Dir = 3;
+  }
+  
+  if(Solution == 1){
+  Dir = 1;
+  }
+ }
+
+ if (HIGH != digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD)){
+   Serial.println("--");
+  Solution = random(1,2);
+  if(Solution == 2){
+  Dir = 2;
+  }
+  
+  if(Solution == 1){
+  Dir = 1;
+  }
+ }
+ if (HIGH == digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD)){
+  Serial.println("|-");
+  Solution = random(1,2);
+  if(Solution == 1){ 
+  Dir = 3;
+  }
+
+  if(Solution == 2){
+  Dir = 2;
+  }
+}
+
+}
+
+ if(HIGH == digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD)){
+  // croissement en X
+  Serial.println("Croissement en X");
+  reflex = random(3);
+  if(reflex == 3){ 
+  Dir = 3;
+  }
+
+  if(reflex == 2){
+  Dir = 2;
+  }
+  
+  if(reflex == 1){
+  Dir = 1;
+  }
+}
+ 
+  /*if(HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAA) && HIGH != digitalRead(signalPinAD)){
   //-|
-  Dir = random(3);
+
   
   }
   if(HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAD) && HIGH != digitalRead(signalPinAA)){
   //--
-  Dir = random(3);
+
   }
   if(HIGH == digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAD) && HIGH != digitalRead(signalPinAG)){
   //|-
-  Dir = random(3);
+
   }
   if(HIGH == digitalRead(signalPinAG) && HIGH == digitalRead(signalPinAA) && HIGH == digitalRead(signalPinAG)){
   //-|-
-  Dir = random(3);
-  }
+  
+  }*/
     
 switch (Dir) {
   case 1 :
-      Motor.speed(MOTOR1, -50);
-      Motor.speed(MOTOR2, 70);
+      Motor.speed(MOTOR1, -40);
+      Motor.speed(MOTOR2, 60);
       Serial.println("Gauche");
 
   break;
 
   case 2 :
-      Motor.speed(MOTOR1, 70);
-      Motor.speed(MOTOR2, -50);
+      Motor.speed(MOTOR1, 60);
+      Motor.speed(MOTOR2, -40);
       Serial.println("Droite");
   break;
 
