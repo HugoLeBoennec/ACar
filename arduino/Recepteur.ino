5e5952a728ca34cdcb3041b309b02c0bbedecb14
yacine-bedrani <33153221+yacine-bedrani@users.noreply.github.com>
2018-06-22 11:14:32 +0200

    Add files via upload
    voila

---
+++ arduino/Recepteur.ino
@@ -0,0 +1,36 @@
+#include <VirtualWire.h> 
+
+int RF_RX_PIN = 4; 
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("setup"); 
+  vw_set_rx_pin(RF_RX_PIN);  // Setup receive pin. 
+  vw_setup(2000); // Transmission speed in bits per second. 
+  vw_rx_start(); // Start the PLL receiver. 
+} 
+
+void loop() 
+{ 
+
+  uint8_t buf[12]; 
+  uint8_t buflen = sizeof buf; 
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+  int i;
+
+        digitalWrite(13, true); // Flash a light to show received good message
+  // Message with a good checksum received, dump it.
+  Serial.print("Got: ");
+
+  for (i = 0; i < buflen; i++)
+  {
+      Serial.print(buf[i], HEX);
+      Serial.print(" ");
+  }
+  Serial.begin(9600);
+  Serial.println("hello");
+        digitalWrite(13, false);
+    }
+}
