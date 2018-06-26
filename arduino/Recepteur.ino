69cde59d838f1423450ee1e6e4c2557c2f90fc6f
Julien Liguori <33621048+julienliguori@users.noreply.github.com>
2018-06-26 09:55:32 +0200

    Update Recepteur.ino

--- arduino/Recepteur.ino
+++ arduino/Recepteur.ino
@@ -1,6 +1,8 @@
 #include <VirtualWire.h> 
 
 int RF_RX_PIN = 6; 
+int num = 0;
+int numero = 1;
 
 void setup() 
 {
@@ -18,6 +20,17 @@ void loop()
   uint8_t buflen = sizeof buf; 
   if (vw_get_message(buf, &buflen)) // Non-blocking
     {
- Serial.println((char *)buf);
+      if (numero == 1){
+        numero = 2;
+        num = atoi((char *)buf);
+        Serial.print("Px = ");
+        Serial.println(num);
+      }
+      else if (numero == 2){
+        numero = 1;
+        num = atoi((char *)buf);
+        Serial.print("Py = ");
+        Serial.println(num);
+      }
     }
 }
